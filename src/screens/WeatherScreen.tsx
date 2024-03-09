98847e5860a60c54f74ef01c77195b9c4b0edd5d
Juan David Caballero <caballeromejiajuandavid@gmail.com>
2024-03-08 19:55:30 -0500

    feat: finish test implementation

--- src/screens/WeatherScreen.tsx
+++ src/screens/WeatherScreen.tsx
@@ -1,8 +1,82 @@
-import React from 'react';
-import {View} from 'react-native';
+import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
+import {
+  ActivityIndicator,
+  Image,
+  ImageProps,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import WeatherService from '../services/weather/WeatherService';
+import {WeatherType} from '../services/weather/WeatherTypes.interface';
+import {RootStackParamList} from '.';
+
+type NavigationProp = RouteProp<RootStackParamList, 'Weather'>;
 
 function WeatherScreen() {
-  return <View testID="weather-screen" />;
+  const [weatherResponse, setWeatherResponse] = useState<WeatherType | null>();
+  const [loading, setLoading] = useState(false);
+  const {goBack} = useNavigation();
+  const route = useRoute<NavigationProp>();
+  const {position} = route.params;
+  useEffect(() => {
+    const fetchWeather = async () => {
+      setLoading(true);
+      const response = await WeatherService.fetchCurrentWeather(
+        position.latitude,
+        position.longitude,
+      );
+      if (response) {
+        setWeatherResponse(response);
+        setLoading(false);
+      }
+    };
+    fetchWeather();
+  }, [position]);
+
+  return (
+    <View testID="weather-screen" style={styles.container}>
+      <TouchableOpacity style={styles.back} onPress={goBack}>
+        <Text style={styles.backText}>Home</Text>
+      </TouchableOpacity>
+      {loading ? (
+        <ActivityIndicator
+          testID="loading-indicator"
+          size="large"
+          color="black"
+        />
+      ) : (
+        <View style={styles.infoContainer}>
+          <Image
+            style={styles.icon}
+            source={{uri: weatherResponse?.icon} as ImageProps}
+          />
+          <Text>{weatherResponse?.city}</Text>
+          <Text>{weatherResponse?.description}</Text>
+          <Text>Stats</Text>
+          <Text>Temperature: {weatherResponse?.temperature}°</Text>
+          <Text>Wind: {weatherResponse?.windSpeed}m/s</Text>
+          <Text>Humidity: {weatherResponse?.humidity}%</Text>
+        </View>
+      )}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 20,
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  back: {position: 'absolute', top: 70, left: 20},
+  backText: {fontSize: 24},
+  icon: {width: 400, height: 400, alignSelf: 'center'},
+  infoContainer: {alignItems: 'center'},
+});
 
 export default WeatherScreen;
--- src/screens/__test__/WeatherScreen.test.tsx
+++ src/screens/__test__/WeatherScreen.test.tsx
@@ -1,10 +1,72 @@
 import React from 'react';
-import {render} from '@testing-library/react-native';
+import {fireEvent, render, waitFor} from '@testing-library/react-native';
 import WeatherScreen from '../WeatherScreen';
+import {useNavigation} from '@react-navigation/native';
+import WeatherService from '../../services/weather/WeatherService';
+import {WeatherType} from '../../services/weather/WeatherTypes.interface';
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    ...jest.requireActual<object>('@react-navigation/native'),
+    useNavigation: jest.fn().mockReturnValue({goBack: jest.fn()}),
+    useRoute: jest
+      .fn()
+      .mockReturnValue({params: {position: {latitude: 0, longitude: 0}}}),
+  };
+});
+
+jest.mock('../../services/weather/WeatherService', () => {
+  return {
+    fetchCurrentWeather: jest.fn(),
+  };
+});
 
 describe('Weather Screen', () => {
   test('should render correctly', () => {
     const wrapper = render(<WeatherScreen />);
     wrapper.getByTestId('weather-screen');
+  });
+
+  test('should return to home screen when back button is pressed', () => {
+    const mockGoBack = jest.fn();
+
+    (useNavigation as jest.Mock).mockReturnValueOnce({goBack: mockGoBack});
+    const wrapper = render(<WeatherScreen />);
+    const backButton = wrapper.getByText('Home');
+    fireEvent.press(backButton);
+    waitFor(() => {
+      expect(mockGoBack).toHaveBeenCalled();
+    });
+  });
+
+  test('should render loading indicator when fetching weather', () => {
+    const wrapper = render(<WeatherScreen />);
+    wrapper.getByTestId('loading-indicator');
+  });
+
+  test('should render weather information when loaded', async () => {
+    const mockWeatherResponse: WeatherType = {
+      city: 'Test City',
+      description: 'Test Description',
+      icon: 'Test Icon',
+      temperature: 20,
+      windSpeed: 10,
+      humidity: 50,
+      pressure: 1000,
+    };
+
+    (WeatherService.fetchCurrentWeather as jest.Mock).mockResolvedValueOnce(
+      mockWeatherResponse,
+    );
+
+    const wrapper = render(<WeatherScreen />);
+
+    await waitFor(() => {
+      wrapper.getByText(mockWeatherResponse.city);
+      wrapper.getByText(mockWeatherResponse.description as string);
+      wrapper.getByText(`Temperature: ${mockWeatherResponse.temperature}°`);
+      wrapper.getByText(`Wind: ${mockWeatherResponse.windSpeed}m/s`);
+      wrapper.getByText(`Humidity: ${mockWeatherResponse.humidity}%`);
+    });
   });
 });
